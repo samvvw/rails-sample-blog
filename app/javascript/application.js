c38df58509f175e41b4cbc51118fe16d0bb961f1
Sam Villegas <45743681+samvvw@users.noreply.github.com>
2022-07-04 20:54:13 -0700

    add E2E tests (#1)

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,3 +1,52 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
-import "@hotwired/turbo-rails"
-import "controllers"
+import '@hotwired/turbo-rails'
+import 'controllers'
+import * as THREE from 'three'
+const main = () => {
+    const canvas = document.querySelector('#c')
+    if (canvas) {
+        const renderer = new THREE.WebGLRenderer({ canvas })
+
+        const fov = 75
+        const aspect = 2
+        const near = 0.1
+        const far = 5
+        const camera = new THREE.PerspectiveCamera(fov, aspect, near, far)
+
+        camera.position.z = 2
+
+        const scene = new THREE.Scene()
+
+        const boxWidth = 1
+        const boxHeight = 1
+        const boxDepth = 1
+        const geometry = new THREE.BoxGeometry(boxWidth, boxHeight, boxDepth)
+
+        const material = new THREE.MeshBasicMaterial({ color: 0x44aa88 })
+
+        const cube = new THREE.Mesh(geometry, material)
+
+        scene.add(cube)
+
+        function render(time) {
+            time *= 0.001
+
+            cube.rotation.x = time
+            cube.rotation.y = time
+
+            renderer.render(scene, camera)
+
+            requestAnimationFrame(render)
+        }
+
+        requestAnimationFrame(render)
+    }
+}
+window.addEventListener('turbo:load', (event) => {
+    console.log('page is fully loaded')
+    main()
+})
+
+// if (document.querySelector('#c')) {
+//     main()
+// }
--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,24 @@
 Rails.application.routes.draw do
   devise_for :admins
-  devise_for :users
+  devise_for :users, controllers: { registrations: 'user/registrations'}
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   root 'articles#index'
 
   # get "/articles", to: "articles#index"
   # get "/articles/:id", to: "articles#show"
+  get '/destroy_all', to: "articles#destroy_all"
+  # resources :articles do
+  # collection do
+  #   get 'destroy_all'
+  # end
+# end
+
+
   resources :articles do
     resources :comments
   end
+
+
 
   # Defines the root path route ("/")
   # root "articles#index"
